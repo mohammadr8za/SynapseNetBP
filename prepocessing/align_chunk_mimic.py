cdd1f1f5a52d4885b0494961ea7695bc8e283e17
mohammadr8za <mohammadreza.peyghan@gmail.com>
2023-06-09 13:03:02 +0430

    change window size to 512 samples

--- prepocessing/align_chunk_mimic.py
+++ prepocessing/align_chunk_mimic.py
@@ -6,14 +6,13 @@ import matplotlib.pyplot as plt
 from scipy.signal import correlate
 
 #     G E N E R A  L     S E T U P
-ppg_save_dir = r'D:\data\PPGBP\mimic\ppg'
-abp_save_dir = r'D:\data\PPGBP\mimic\abp'
+ppg_save_dir = r'D:\PPG2ABP\DenoisingNetwork\DATA\initial_ppg_abp\512samples\ppg'
+abp_save_dir = r'D:\PPG2ABP\DenoisingNetwork\DATA\initial_ppg_abp\512samples\abp'
 fs = 125
 T = 1 / fs
-chunk_in_sec = 5
-chunk_in_sample = int(5 / T) # اینجا به جای 5 ، chunk in sec گذاشتم
-
-annots_1 = loadmat('Part_1.mat')
+chunk_in_sec = 4.096
+chunk_in_sample = int(chunk_in_sec / T)
+annots_1 = loadmat('D:\PythonProjects\PPGBP\Part_1.mat')
 part_1 = annots_1['Part_1'].squeeze()
 
 # annots_3 = loadmat('Part_3.mat')
@@ -22,7 +21,7 @@ part_1 = annots_1['Part_1'].squeeze()
 # annots_4 = loadmat('Part_4.mat')
 # part_4 = annots_4['Part_4'].squeeze()
 
-for sig in range(111, len(part_1)):
+for sig in range(len(part_1)):
     ppg_sig = part_1[sig][0]
     abp_sig = part_1[sig][1]
     # Alignment
@@ -37,9 +36,9 @@ for sig in range(111, len(part_1)):
         ppg_chunk = ppg_sig[num * chunk_in_sample: (num + 1) * chunk_in_sample]
         plt.figure()
         plt.plot(np.arange(0, ppg_chunk.shape[0]), ppg_chunk)
-        plt.savefig(os.path.join(ppg_save_dir, chunk_name) + '.png')
+        plt.savefig(os.path.join(ppg_save_dir, 'fig', chunk_name) + '.png')
         plt.close()
-        np.savetxt(os.path.join(ppg_save_dir, chunk_name) + '.txt', ppg_chunk, fmt='%f')
+        np.savetxt(os.path.join(ppg_save_dir, 'txt', chunk_name) + '.txt', ppg_chunk, fmt='%f')
 
         abp_chunk = abp_sig[num * chunk_in_sample: (num + 1) * chunk_in_sample]
         # plt.figure()
