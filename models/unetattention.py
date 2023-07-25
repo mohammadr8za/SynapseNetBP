e1b9bab3ed81fef58daa8a0539e39574ed552e12
Mohammad Fallahi <fallahimohammad96@gmail.com>
2023-07-25 20:19:34 +0330

    modification shapes of input and outputs

--- models/unetattention.py
+++ models/unetattention.py
@@ -11,12 +11,12 @@ class AttentionBlock(nn.Module):
         self.conv3 = nn.Conv1d(in_channels // 2, 1, kernel_size=1)
 
     def forward(self, x):
-        query = self.conv1(x)
-        key = self.conv2(x)
+        query = self.conv1(x.unsqueeze(0))  # Add extra batch dimension
+        key = self.conv2(x.unsqueeze(0))  # Add extra batch dimension
         energy = torch.matmul(query.transpose(2, 1), key)
         attention_weights = F.softmax(energy, dim=2)
-        attended = torch.matmul(attention_weights, x)
-        output = self.conv3(attended)
+        attended = torch.matmul(attention_weights, x.unsqueeze(0))  # Add extra batch dimension
+        output = self.conv3(attended).squeeze(0)  # Remove extra batch dimension
         return output
 
 
@@ -50,34 +50,38 @@ class UNetPPGtoABP(nn.Module):
 
     def forward(self, x):
         # Encoder part of the network
-        x1 = F.relu(self.conv1(x))
-        x1 = self.attention1(x1)
+        x1 = F.relu(self.conv1(x.unsqueeze(0)))  # Add extra batch dimension
+        x1 = self.attention1(x1).squeeze(0)  # Remove extra batch dimension
 
-        x2 = F.relu(self.conv2(F.max_pool1d(x1, kernel_size=2, stride=2)))
-        x2 = self.attention2(x2)
+        x2 = F.relu(self.conv2(F.max_pool1d(x1.unsqueeze(0), kernel_size=2, stride=2)))  # Add extra batch dimension
+        x2 = self.attention2(x2).squeeze(0)  # Remove extra batch dimension
 
-        x3 = F.relu(self.conv3(F.max_pool1d(x2, kernel_size=2, stride=2)))
-        x3 = self.attention3(x3)
+        x3 = F.relu(self.conv3(F.max_pool1d(x2.unsqueeze(0), kernel_size=2, stride=2)))  # Add extra batch dimension
+        x3 = self.attention3(x3).squeeze(0)  # Remove extra batch dimension
 
-        x4 = F.relu(self.conv4(F.max_pool1d(x3, kernel_size=2, stride=2)))
-        x4 = self.attention4(x4)
+        x4 = F.relu(self.conv4(F.max_pool1d(x3.unsqueeze(0), kernel_size=2, stride=2)))  # Add extra batch dimension
+        x4 = self.attention4(x4).squeeze(0)  # Remove extra batch dimension
 
-        x5 = F.relu(self.conv5(F.max_pool1d(x4, kernel_size=2, stride=2)))
-        x5 = self.dropout(x5)
+        x5 = F.relu(self.conv5(F.max_pool1d(x4.unsqueeze(0), kernel_size=2, stride=2))).squeeze(
+            0)  # Removeextra batch dimension
 
         # Decoder part of the network
-        x = F.relu(self.upconv1(x5))
-        x = torch.cat([x, x4], dim=1)
-        x = F.relu(self.conv6(x))
-        x = F.relu(self.upconv2(x))
-        x = torch.cat([x, x3], dim=1)
-        x = F.relu(self.conv7(x))
-        x = F.relu(self.upconv3(x))
-        x = torch.cat([x, x2], dim=1)
-        x = F.relu(self.conv8(x))
-        x = F.relu(self.upconv4(x))
-        x = torch.cat([x, x1], dim=1)
-        x = F.relu(self.conv9(x))
-        x = self.conv10(x)
+        x6 = self.dropout(F.relu(self.upconv1(x5)))
+        x6 = torch.cat((x6, x4), dim=1)
+        x6 = self.dropout(F.relu(self.conv6(x6)))
 
-        return x.squeeze(1)
+        x7 = self.dropout(F.relu(self.upconv2(x6)))
+        x7 = torch.cat((x7, x3), dim=1)
+        x7 = self.dropout(F.relu(self.conv7(x7)))
+
+        x8 = self.dropout(F.relu(self.upconv3(x7)))
+        x8 = torch.cat((x8, x2), dim=1)
+        x8 = self.dropout(F.relu(self.conv8(x8)))
+
+        x9 = self.dropout(F.relu(self.upconv4(x8)))
+        x9 = torch.cat((x9, x1), dim=1)
+        x9 = self.dropout(F.relu(self.conv9(x9)))
+
+        output = self.conv10(x9).squeeze(1)  # Adjust the dimensions
+
+        return output
